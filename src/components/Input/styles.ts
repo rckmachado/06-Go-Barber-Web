b7a34cbec71abe9be26c33306a5f40e96adbebed
Rick M <contato.rickmachado@gmail.com>
2020-10-28 10:07:46 -0300

    ...

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,4 +1,49 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+    isFocused: boolean;
+    isField: boolean;
+}
 
 // eslint-disable-next-line import/prefer-default-export
-export const Container = styled.input``;
+export const Container = styled.div<ContainerProps>`
+    background: #232129;
+    border-radius: 10px;
+    border: 2px solid #232129;
+    padding: 16px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    color: #666360;
+
+    & + div {
+        margin-top: 8px;
+    }
+
+    ${props =>
+        props.isFocused &&
+        css`
+            color: #ff9000;
+            border-color: #ff9000;
+        `}
+
+    ${props =>
+        props.isField &&
+        css`
+            color: #ff9000;
+        `}
+
+    input {
+        flex: 1;
+        background: transparent;
+        color: #f4e0e8;
+        border: 0;
+        &::placeholder {
+            color: #666360;
+        }
+    }
+
+    svg {
+        margin-right: 16px;
+    }
+`;
