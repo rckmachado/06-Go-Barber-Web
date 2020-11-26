5554922506237caf3f25a32a2e0febfe09e28cf4
Rick M <contato.rickmachado@gmail.com>
2020-11-26 19:59:20 -0300

    ...

--- src/components/toasts/index.tsx
+++ src/components/toasts/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
+import { useTransition } from 'react-spring';
 
 import Toast from './toast';
 
@@ -12,10 +13,20 @@ interface ToastContainerProps {
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
+    const messagesWithTransitions = useTransition(
+        messages,
+        message => message.id,
+        {
+            from: { right: '-120%', opacity: 0 },
+            enter: { right: '0%', opacity: 1 },
+            leave: { right: '-120%', opacity: 0 },
+        },
+    );
+
     return (
         <Container>
-            {messages.map(message => (
-                <Toast key={message.id} message={message} />
+            {messagesWithTransitions.map(({ item, key, props }) => (
+                <Toast key={key} style={props} message={item} />
             ))}
         </Container>
     );
---
+++ src/routes/Route.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    Route as ReactDOMRoute,
+    RouteProps as ReactDOMRouterProps,
+    Redirect,
+} from 'react-router-dom';
+import { boolean } from 'yup';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouterProps extends ReactDOMRouterProps {
+    isPrivate?: boolean;
+    component: React.ComponentType;
+}
+
+const Route: React.FC<RouterProps> = ({
+    isPrivate = false,
+    component: Component,
+    ...rest
+}) => {
+    const { user } = useAuth();
+
+    return (
+        <ReactDOMRoute
+            {...rest}
+            render={({ location }) => {
+                return isPrivate === !!user ? (
+                    <Component />
+                ) : (
+                    <Redirect
+                        to={{
+                            pathname: isPrivate ? `/` : `/dashboard`,
+                            state: { from: location },
+                        }}
+                    />
+                );
+            }}
+        />
+    );
+};
+
+export default Route;
